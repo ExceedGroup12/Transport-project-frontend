7a58f9cf2dceca901cadad2060980274ee7aeb4d
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-18 02:15:00 +0700

    modified file and add dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import { Home, StationDetail } from './pages';
+import { Navbar } from './components';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/:station/detail' element={<StationDetail />} />
